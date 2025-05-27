4331c343992839687a67ca8ee6459b1b338352c2
Haaju14 <phuchau22923@gmail.com>
2025-05-27 16:32:28 +0700

    sv

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,7 @@
 import { Sequelize } from 'sequelize';
     import dotenv from 'dotenv';
 
-    dotenv.config();
-
+dotenv.config({ path: '/home/master/cholimex/DATT_BE/.env' });
     const sequelize = new Sequelize(
       process.env.DB_DATABASE,
       process.env.DB_USER,
